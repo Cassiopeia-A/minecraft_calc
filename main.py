555905388fcfc255c57c34469d8808764988230e
Cassiopeia-A <sdfghjkl246@mail.ru>
2023-11-05 00:13:47 +0500

    b

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import sqlite3
 from PyQt5 import uic
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QSize
-from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog
+from PyQt5.QtWidgets import QApplication, QMainWindow, QInputDialog, QButtonGroup, QMessageBox
 
 
 class Calculator(QMainWindow):
@@ -14,29 +14,29 @@ class Calculator(QMainWindow):
         self.req_list = {}
         self.def_icon = self.button_item_1.icon()
         self.images_list = [''] * 7
+        self.res_items = []
+        self.spin_boxes = [self.item_count_1, self.item_count_2, self.item_count_3, self.item_count_4,
+                           self.item_count_5, self.item_count_6, self.item_count_7]
+        self.result_btns = [self.item_1, self.item_2, self.item_3, self.item_4, self.item_5, self.item_6, self.item_7,
+                            self.item_8, self.item_9, self.item_10, self.item_11, self.item_12, self.item_13,
+                            self.item_14, self.item_15, self.item_16, self.item_17, self.item_18, self.item_19,
+                            self.item_20, self.item_21, self.item_22]
+        self.count_labels = [self.count_1, self.count_2, self.count_3, self.count_4, self.count_5, self.count_6,
+                             self.count_7, self.count_8, self.count_9, self.count_10, self.count_11, self.count_12,
+                             self.count_13, self.count_14, self.count_15, self.count_16, self.count_17,
+                             self.count_18, self.count_19, self.count_20, self.count_21, self.count_22]
 
         self.con = sqlite3.connect("mine_calc.sql")
         self.cur = self.con.cursor()
 
-        self.close_1.hide()
-        self.close_2.hide()
-        self.close_3.hide()
-        self.close_4.hide()
-        self.close_5.hide()
-        self.close_6.hide()
         self.button_item_2.hide()
         self.button_item_3.hide()
         self.button_item_4.hide()
         self.button_item_5.hide()
         self.button_item_6.hide()
         self.button_item_7.hide()
-        self.item_count_1.hide()
-        self.item_count_2.hide()
-        self.item_count_3.hide()
-        self.item_count_4.hide()
-        self.item_count_5.hide()
-        self.item_count_6.hide()
-        self.item_count_7.hide()
+        for spinbox in self.spin_boxes:
+            spinbox.hide()
 
         self.add_item.clicked.connect(self.to_page_2)
         self.back_btn.clicked.connect(self.to_page_1)
@@ -46,20 +46,46 @@ class Calculator(QMainWindow):
         self.add_craft.clicked.connect(self.add_craft_f)
         self.calculate.clicked.connect(self.count)
 
-        self.button_item_1.clicked.connect(self.choose_item_1)
-        self.button_item_2.clicked.connect(self.choose_item_2)
-        self.button_item_3.clicked.connect(self.choose_item_3)
-        self.button_item_4.clicked.connect(self.choose_item_4)
-        self.button_item_5.clicked.connect(self.choose_item_5)
-        self.button_item_6.clicked.connect(self.choose_item_6)
-        self.button_item_7.clicked.connect(self.choose_item_7)
+        self.button_item_1.clicked.connect(self.choose_item)
+        self.button_item_2.clicked.connect(self.choose_item)
+        self.button_item_3.clicked.connect(self.choose_item)
+        self.button_item_4.clicked.connect(self.choose_item)
+        self.button_item_5.clicked.connect(self.choose_item)
+        self.button_item_6.clicked.connect(self.choose_item)
+        self.button_item_7.clicked.connect(self.choose_item)
 
-        self.close_1.clicked.connect(self.close_f_1)
-        self.close_2.clicked.connect(self.close_f_2)
-        self.close_3.clicked.connect(self.close_f_3)
-        self.close_4.clicked.connect(self.close_f_4)
-        self.close_5.clicked.connect(self.close_f_5)
-        self.close_6.clicked.connect(self.close_f_6)
+        self.close_1.clicked.connect(self.close_f)
+        self.close_2.clicked.connect(self.close_f)
+        self.close_3.clicked.connect(self.close_f)
+        self.close_4.clicked.connect(self.close_f)
+        self.close_5.clicked.connect(self.close_f)
+        self.close_6.clicked.connect(self.close_f)
+
+        self.item_group_btn = QButtonGroup()
+        self.item_group_btn.addButton(self.button_item_1, id=1)
+        self.item_group_btn.addButton(self.button_item_2, id=2)
+        self.item_group_btn.addButton(self.button_item_3, id=3)
+        self.item_group_btn.addButton(self.button_item_4, id=4)
+        self.item_group_btn.addButton(self.button_item_5, id=5)
+        self.item_group_btn.addButton(self.button_item_6, id=6)
+        self.item_group_btn.addButton(self.button_item_7, id=7)
+
+        self.close_group_btn = QButtonGroup()
+        self.close_group_btn.addButton(self.close_1, id=1)
+        self.close_group_btn.addButton(self.close_2, id=2)
+        self.close_group_btn.addButton(self.close_3, id=3)
+        self.close_group_btn.addButton(self.close_4, id=4)
+        self.close_group_btn.addButton(self.close_5, id=5)
+        self.close_group_btn.addButton(self.close_6, id=6)
+        for button in self.close_group_btn.buttons():
+            button.hide()
+
+        self.result_group_btn = QButtonGroup()
+        for index, res in enumerate(self.result_btns):
+            self.result_group_btn.addButton(res, id=index + 1)
+            res.clicked.connect(self.info)
+            res.hide()
+            self.count_labels[index].hide()
 
     def get_items(self):
         items = self.cur.execute('''SELECT name FROM resourses''').fetchall()
@@ -92,10 +118,12 @@ class Calculator(QMainWindow):
         image = self.image_name.text()
 
         if item not in list_items:
-            if item != '':
-                self.cur.execute(f"""INSERT INTO resourses(name) VALUES('{item.lower()}')""")
+            if item != '' or image != '':
+                self.cur.execute(f"""INSERT INTO resourses(name, image) VALUES('{item.lower()}', '{image}')""")
                 self.con.commit()
                 self.statusBar().showMessage(f'Предмет {item.lower()} успешно добавлен')
+            else:
+                self.statusBar().showMessage('Ошибка')
         else:
             self.cur.execute(f'''UPDATE resourses SET image = "{image}" WHERE name = "{item}"''')
             self.con.commit()
@@ -155,10 +183,12 @@ class Calculator(QMainWindow):
 
     def count(self):
         res_dict = dict()
-        count = [self.item_count_1.value(), self.item_count_2.value(), self.item_count_3.value(),
-                 self.item_count_4.value(), self.item_count_5.value(), self.item_count_6.value(),
-                 self.item_count_7.value()]
-        for i, elem in enumerate(self.images_list):
+        self.res_items.clear()
+        count_items = [self.item_count_1.value(), self.item_count_2.value(), self.item_count_3.value(),
+                       self.item_count_4.value(), self.item_count_5.value(), self.item_count_6.value(),
+                       self.item_count_7.value()]
+        items = self.images_list.copy()
+        for i, elem in enumerate(items):
             if elem:
                 keys = self.cur.execute(f'''SELECT requirement_items_id FROM crafts
                 WHERE result_item_id =
@@ -168,162 +198,69 @@ class Calculator(QMainWindow):
                 (SELECT id FROM resourses WHERE image = "{elem}")''').fetchall()[0][0].split(', ')
                 for index, k in enumerate(keys):
                     key = self.cur.execute(f'''SELECT name FROM resourses WHERE id = "{k}"''').fetchall()[0][0]
-                    if key not in res_dict.keys():
-                        res_dict[key] = int(values[index]) * count[i]
+                    if key not in self.get_items_with_craft():
+                        if key not in res_dict.keys():
+                            res_dict[key] = int(values[index]) * count_items[i]
+                        else:
+                            res_dict[key] = res_dict.get(key) + int(values[index]) * count_items[i]
                     else:
-                        res_dict[key] = res_dict.get(key) + int(values[index]) * count[i]
-        print(res_dict)
+                        count_items.append(int(values[index]) * count_items[i])
+                        img = self.cur.execute(f'''SELECT image FROM resourses WHERE name = "{key}"''').fetchall()[0][0]
+                        items.append(img)
 
-    def choose_item_1(self):
+        result = 0
+        for j, r in enumerate(self.result_btns):
+            r.hide()
+            self.count_labels[j].hide()
+        for item, count in res_dict.items():
+            image = self.cur.execute(f'''SELECT image FROM resourses WHERE name = "{item}"''').fetchall()
+            self.result_group_btn.button(result + 1).show()
+            self.result_group_btn.button(result + 1).setIcon(QIcon(f'images/{image[0][0]}'))
+            self.result_group_btn.button(result + 1).setIconSize(QSize(55, 55))
+            self.count_labels[result].show()
+            self.count_labels[result].setText(f'x {int(count)}')
+            self.res_items.append(item)
+            result += 1
+
+    def info(self):
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        i = self.result_group_btn.id(self.sender()) - 1
+        msg.setText(f'Этот предмет: {self.res_items[i]}')
+        msg.setWindowTitle('Информация о предмете')
+        msg.setStandardButtons(QMessageBox.Ok)
+        msg.exec()
+
+    def choose_item(self):
         items = self.get_items_with_craft()
 
         item, ok = QInputDialog().getItem(self, "Выберети предмет",
                                           "Предметы:", items, 0, False)
         if ok and item != '':
+            i = self.item_group_btn.id(self.sender())
             path = self.cur.execute(f'''SELECT image FROM resourses WHERE name = "{item}"''').fetchall()
-            self.button_item_1.setIcon(QIcon(f'images/{path[0][0]}'))
+            self.sender().setIcon(QIcon(f'images/{path[0][0]}'))
             self.sender().setIconSize(QSize(90, 90))
-            self.button_item_2.show()
-            self.item_count_1.show()
-            self.images_list[0] = f'{path[0][0]}'
+            if i != 7:
+                self.item_group_btn.button(i + 1).show()
+            if i > 1:
+                for button in self.close_group_btn.buttons():
+                    button.hide()
+                self.close_group_btn.button(i - 1).show()
+            self.spin_boxes[i - 1].show()
+            self.images_list[i - 1] = f'{path[0][0]}'
 
-    def choose_item_2(self):
-        items = self.get_items_with_craft()
-
-        item, ok = QInputDialog().getItem(self, "Выберети предмет",
-                                          "Предметы:", items, 0, False)
-        if ok and item != '':
-            path = self.cur.execute(f'''SELECT image FROM resourses WHERE name = "{item}"''').fetchall()
-            self.button_item_2.setIcon(QIcon(f'images/{path[0][0]}'))
-            self.sender().setIconSize(QSize(90, 90))
-            self.button_item_3.show()
-            self.item_count_2.show()
-            self.images_list[1] = f'{path[0][0]}'
-            if self.button_item_4.isHidden():
-                self.close_1.show()
-
-    def choose_item_3(self):
-        items = self.get_items_with_craft()
-
-        item, ok = QInputDialog().getItem(self, "Выберети предмет",
-                                          "Предметы:", items, 0, False)
-        if ok and item != '':
-            path = self.cur.execute(f'''SELECT image FROM resourses WHERE name = "{item}"''').fetchall()
-            self.button_item_3.setIcon(QIcon(f'images/{path[0][0]}'))
-            self.sender().setIconSize(QSize(90, 90))
-            self.button_item_4.show()
-            self.close_1.hide()
-            self.item_count_3.show()
-            self.images_list[2] = f'{path[0][0]}'
-            if self.button_item_5.isHidden():
-                self.close_2.show()
-
-    def choose_item_4(self):
-        items = self.get_items_with_craft()
-
-        item, ok = QInputDialog().getItem(self, "Выберети предмет",
-                                          "Предметы:", items, 0, False)
-        if ok and item != '':
-            path = self.cur.execute(f'''SELECT image FROM resourses WHERE name = "{item}"''').fetchall()
-            self.button_item_4.setIcon(QIcon(f'images/{path[0][0]}'))
-            self.sender().setIconSize(QSize(90, 90))
-            self.button_item_5.show()
-            self.close_2.hide()
-            self.item_count_4.show()
-            self.images_list[3] = f'{path[0][0]}'
-            if self.button_item_6.isHidden():
-                self.close_3.show()
-
-    def choose_item_5(self):
-        items = self.get_items_with_craft()
-
-        item, ok = QInputDialog().getItem(self, "Выберети предмет",
-                                          "Предметы:", items, 0, False)
-        if ok and item != '':
-            path = self.cur.execute(f'''SELECT image FROM resourses WHERE name = "{item}"''').fetchall()
-            self.button_item_5.setIcon(QIcon(f'images/{path[0][0]}'))
-            self.sender().setIconSize(QSize(90, 90))
-            self.button_item_6.show()
-            self.close_3.hide()
-            self.item_count_5.show()
-            self.images_list[4] = f'{path[0][0]}'
-            if self.button_item_7.isHidden():
-                self.close_4.show()
-
-    def choose_item_6(self):
-        items = self.get_items_with_craft()
-
-        item, ok = QInputDialog().getItem(self, "Выберети предмет",
-                                          "Предметы:", items, 0, False)
-        if ok and item != '':
-            path = self.cur.execute(f'''SELECT image FROM resourses WHERE name = "{item}"''').fetchall()
-            self.button_item_6.setIcon(QIcon(f'images/{path[0][0]}'))
-            self.sender().setIconSize(QSize(90, 90))
-            self.button_item_7.show()
-            self.close_4.hide()
-            self.close_5.show()
-            self.item_count_6.show()
-            self.images_list[5] = f'{path[0][0]}'
-
-    def choose_item_7(self):
-        items = self.get_items_with_craft()
-
-        item, ok = QInputDialog().getItem(self, "Выберети предмет",
-                                          "Предметы:", items, 0, False)
-        if ok and item != '':
-            path = self.cur.execute(f'''SELECT image FROM resourses WHERE name = "{item}"''').fetchall()
-            self.button_item_7.setIcon(QIcon(f'images/{path[0][0]}'))
-            self.sender().setIconSize(QSize(90, 90))
-            self.close_5.hide()
-            self.close_6.show()
-            self.item_count_7.show()
-            self.images_list[6] = f'{path[0][0]}'
-
-    def close_f_1(self):
-        self.button_item_2.setIcon(self.def_icon)
-        self.button_item_3.hide()
-        self.close_1.hide()
-        self.item_count_2.hide()
-        self.images_list[1] = ''
-
-    def close_f_2(self):
-        self.button_item_3.setIcon(self.def_icon)
-        self.button_item_4.hide()
-        self.close_2.hide()
-        self.close_1.show()
-        self.item_count_3.hide()
-        self.images_list[2] = ''
-
-    def close_f_3(self):
-        self.button_item_4.setIcon(self.def_icon)
-        self.button_item_5.hide()
-        self.close_3.hide()
-        self.close_2.show()
-        self.item_count_4.hide()
-        self.images_list[3] = ''
-
-    def close_f_4(self):
-        self.button_item_5.setIcon(self.def_icon)
-        self.button_item_6.hide()
-        self.close_4.hide()
-        self.close_3.show()
-        self.item_count_5.hide()
-        self.images_list[4] = ''
-
-    def close_f_5(self):
-        self.button_item_6.setIcon(self.def_icon)
-        self.button_item_7.hide()
-        self.close_5.hide()
-        self.close_4.show()
-        self.item_count_6.hide()
-        self.images_list[5] = ''
-
-    def close_f_6(self):
-        self.button_item_7.setIcon(self.def_icon)
-        self.close_6.hide()
-        self.close_5.show()
-        self.item_count_7.hide()
-        self.images_list[6] = ''
+    def close_f(self):
+        i = self.close_group_btn.id(self.sender())
+        self.item_group_btn.button(i + 1).setIcon(self.def_icon)
+        if i != 6:
+            self.item_group_btn.button(i + 2).hide()
+        self.sender().hide()
+        if i != 1:
+            self.close_group_btn.button(i - 1).show()
+        self.spin_boxes[i].setValue(1)
+        self.spin_boxes[i].hide()
+        self.images_list[i] = ''
 
     def closeEvent(self, event):
         self.con.close()
---
+++ test.py
@@ -0,0 +1,3 @@
+d = ['adwwa', 2, 6, 'awdaw', [343, 'awd']]
+for index, elem in enumerate(d):
+    print(index, elem)
